b3833c87683330970ffff6a2ef60772fecaddff2
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-11-13 16:12:54 -1000

    Several fixes + new feature
    Added a search feature.
    Fixed some styling.
    Separated name and surname for the add.

--- templates/styling.js
+++ templates/styling.js
@@ -4,16 +4,16 @@ function stringToHslColor(str,s,l) {var h=0; for (var i=0; i<str.length; i++) {h
 function loadStyles() {
 //Loaded elements dependent
 var clc=document.getElementById("contactsListContainer")
-if (clc!=null) {/*We can set up the styling*/ var sh=clc.offsetHeight; var d=clc.parentElement; if (d!=null) {var m = d.parentElement; if (m!=null)  {
-var mh=m.offsetHeight; if (mh<=sh) {clc.classList.remove("cLCEnableAnim");}}}}
+if (clc!=null) {/*We can set up the styling*/ var sh=clc.offsetHeight; var d=clc.parentElement; if (d!=null) {var m=d.parentElement; if (m!=null) {var mh=m.offsetHeight; if (mh<=sh) {clc.classList.remove("cLCEnableAnim");}}}}
 
 //Value dependent
 var p=document.getElementById("Profile-Picture");
-if (p!=null) {console.log("Found a picture");var cn=document.getElementById("Contact-Name"); if (cn!=null) {console.log("Found the name"); Object.assign(p.style, {"background-color": stringToHslColor(cn.innerHTML, "68", "70")});}}
+if (p!=null) {var cn=document.getElementById("Contact-Name"); if (cn!=null) {Object.assign(p.style, {"background-color": stringToHslColor(cn.innerHTML, "68", "70")});}}
 }
 window.addEventListener("load", loadStyles);
 
 var btns=document.getElementsByClassName("back-home");
-for (var i = 0; i < btns.length; i++) {
-   btns.item(i).href = "index.html"; 
-}
+for (var i=0; i<btns.length; i++) {btns.item(i).action = "index.html";}
+
+//Search feature for index.html
+function updateSearch(data) {var ses=document.getElementsByClassName("SE"); if (data!="") {/*Get all elements*/for (var i=0; i<ses.length; i++) {var it = ses.item(i); if (it.innerHTML.includes(data)) {/*To ensure it is visible*/it.parentElement.parentElement.classList.remove("hidden");} else {it.parentElement.parentElement.classList.add("hidden");}}} else {/*Just show it all*/for (var i = 0; i < ses.length; i++) {ses.item(i).parentElement.parentElement.classList.remove("hidden");}}}
